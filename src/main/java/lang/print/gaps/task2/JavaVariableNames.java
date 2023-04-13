063920c979048a2371ec277e7e163622e78d3b15
Muzaffar55 <97522350+Muzaffar55@users.noreply.github.com>
2023-04-13 09:34:14 +0500

    Updated

--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class JavaVariableNames {
     public static void main(String[] args) {
-        int morrowing = 0;
-        int moneyDollar = 10;
-        int hundredPercent = 100;
-        int backOn = 1000;
-        int underscored = 10000;
-        int sharply = 100000;
-        int starts = 1000000;
-        int badExampleForVarName = 1000000;
+        int morrow = 0;
+        int money = 10;
+        int hundred = 100;
+        int back = 1000;
+        int underscore = 10000;
+        int sharp = 100000;
+        int star = 1000000;
+        int goodExample = 1000000;
     }
 }
